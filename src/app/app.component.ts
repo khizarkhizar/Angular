675717f4c0b5d0abac7b325e4b8e48d079552268
khizarkhizar <khizarrana756@gmail.com>
2019-08-26 16:44:06 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+declare function require(path: string);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = 'LearnAngular';
+  public hasError=false;
+ public imageSrc = require('src/Images/logo.jpg');
+ public imageSrc1 = require('src/Images/logo.jpg');
+  imageSrc2 = require('src/Images/logo.jpg');
+  imageSrc3 = require('src/Images/logo.jpg');
+  imageSrc4 = require('src/Images/logo.jpg');
+  imageSrc5 = require('src/Images/logo.jpg');
+  imageSrc6 = require('src/Images/logo.jpg');
+ public images=["imageSrc","imageSrc1","imageSrc2"];
+  title = 'Umer Abdullah:How to Chill?';
+  kd=alert("Welcome to jani's world");
+  onClick(value){
+    if(value=='jani book'){ console.log('welcome jani hahah');
+    
+  }
+    
+  }
 }
